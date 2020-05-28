167b870cf3fad86d6bb5e288afaa178c56c10e69
Jake Diorio <jdiorio2393@gmail.com>
2020-05-28 17:02:34 -0400

    state changes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,33 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, Component} from 'react'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('Order Summary Updated')
+    }
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
         return( 
         <li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-        </li>
-    )
-} )
-    return (
-        <Fragment>
+            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+        </li> 
+        )
+    })
+        return(
+            <Fragment>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingedients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to checkout</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Fragment>
-    )
+        )
+    }
+    
 }
-
-export default orderSummary
+export default OrderSummary
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/auxiliary'
 import classes from './layout.css'
 import Toolbar from "../Navigation/Toolbar/Toolbar"
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-    <main className={classes.content}>
-        {props.children}
-    </main>
-    </Aux>
-)
+class Layout extends Component {
+    
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    sideDrawerCLosedHandler = () => {
+       this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+           return {showSideDrawer: !prevState.showSideDrawer}
+    }) 
+}
+
+    render() {
+        return(
+    <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer 
+        closed={this.sideDrawerCLosedHandler}
+        open={this.state.showSideDrawer}/>
+        <main className={classes.content}>
+            {this.props.children}
+        </main>
+    </Aux>
+        )
+    }
+}
+export default Layout;
